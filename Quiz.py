52e894508cbcbfb0c0f0751a9e27066ed04a4273
SUPERSAM136 <80008441+SUPERSAM136@users.noreply.github.com>
2022-06-23 17:22:30 +0100

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,31 @@
+print("Welcome to the quiz!")
+Q1=input("Question 1: How many brain cells does Mona have? Say your answer as a number: ")
+if Q1=="0":
+    print("You are correct!")
+else:
+    print("You are wrong, idiot, Mona has 0 brain cells!")
+        
+Q2=input("Question 2: What subject is better: Maths, English, Science or Art? Make sure to include a capital letter at the start of your answer: ")
+if Q2=="Maths":
+    print("Well done, you are correct!")
+else:
+    print("You are wrong, idiot, it's Maths!")
+
+Q3=input("Question 3: I want to seperate salt and sand. What seperation method should I use? Chromatography, Filtration, Crystallisation or Distillation? Make sure to include a capital letter in your answer: ")
+if Q3=="Filtration":
+    print("Congratulations, you are correct!")
+else:
+    print("You are wrong, dumbo, it's Filtration!")
+
+Q4=input("Question 4: What is 4+7-(9/3)+(3+96)? Is it 91, 87, -92 or -91? Choose one of those answers and type it as shown (as a number): ")
+if Q4=="-91":
+    print("Really well done, you are correct!")
+else:
+    print("You're wrong, but I'll allow it this time as it was a hard question (the answer was -91).")
+
+Q5=input("Question 5: Which one is better: Marvel, DC or Star Wars? Make sure to include all the capital letters in your answer: ")
+if Q5=="Marvel":
+    print("Nice, you got it correct! (Despite it being an easy question.)")
+else:
+    print("WRONG?! THAT WAS SO EASY!!! HOW COULD YOU NOT PICK MARVEL?!")
+    
